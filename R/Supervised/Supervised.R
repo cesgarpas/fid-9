561827571fb3e0b5f92bebead649af93e86613e5
CesarGP <cgpcsjcmp@gmail.com>
2021-01-17 20:50:40 +0100

    feat: unsupervised

--- R/Supervised/Supervised.R
+++ R/Supervised/Supervised.R
@@ -1,10 +1,11 @@
 # Libraries
 install.package("caret")
 install.package("dplyr")
+install.packages("rattle")
+
 library(caret)
 library(dplyr)
 
-install.packages("rattle")
 library(rpart)
 library(rattle)
 library(RColorBrewer)
@@ -22,46 +23,44 @@ print(disctinct_platforms)
 # Group by name
 vgsales_preprocessed_concatenated_platform_dummies <- vgsales_preprocessed_concatenated_platform
 
-# Create dummy variables depending on if the attribute Concatenate.Platform contains the platform name previously distincted
+# Create dummy variables depending on 
+#   if the attribute Concatenate.Platform contains the platform name previously distincted
+#   if it is a "superventas" 
 for (i in 1:length(disctinct_platforms[,])) {
   platform_dummy_name <- paste("Platform-", disctinct_platforms[i,], sep="")
   
   vgsales_preprocessed_concatenated_platform_dummies <-
     vgsales_preprocessed_concatenated_platform_dummies %>%
     mutate(!!toString(platform_dummy_name) := 
-             (grepl(disctinct_platforms[i,], vgsales_preprocessed_concatenated_platform_dummies$Concatenate.Platform, fixed = TRUE)))
+             (grepl(disctinct_platforms[i,], vgsales_preprocessed_concatenated_platform_dummies$Concatenate.Platform, fixed = TRUE))) %>% 
+    mutate(Supersale = (vgsales_preprocessed_concatenated_platform_dummies$Sum.Global_Sales. >= 0.2))
 }
 
+head(vgsales_preprocessed_concatenated_platform_dummies)
 
 # Delete unused variables
 vgsales_preprocessed_concatenated_platform_dummies <- select(vgsales_preprocessed_concatenated_platform_dummies, -c(Concatenate.Platform., row.ID))
 
-print(vgsales_preprocessed_concatenated_platform_dummies[2,])
-head(vgsales_preprocessed_concatenated_platform_dummies)
-
 ####################### Training ####################### 
 
-train<-read.delim("data/train.csv", sep = "\t", head = TRUE)
-rownames(train) <- train$id
-train$id <- NULL
-
-train <- 
-
+# Split data into training and testing set
 set.seed(1)
+dt <- sort(sample(nrow(vgsales_preprocessed_concatenated_platform_dummies), nrow(vgsales_preprocessed_concatenated_platform_dummies)*.9))
+train_set<-vgsales_preprocessed_concatenated_platform_dummies[dt,]
+test_set<-vgsales_preprocessed_concatenated_platform_dummies[-dt,]
 
-tree <- rpart(Survived~., train, method = "class")
+# Remove not useful columns
+train_set <- select(train_set, -c(Name, First.Publisher., Sum.NA_Sales., Sum.EU_Sales., Sum.JP_Sales., Sum.Other_Sales., Sum.Global_Sales., Min..Year.))
 
+# Train and plot tree
+tree <- rpart(Supersale ~ ., train_set, method = "class")
 fancyRpartPlot(tree)
 
 
-test<-read.delim("data/test.csv", sep = "\t", head = TRUE)
-rownames(test) <- test$id
-test$id <- NULL
-
-pred <- predict(tree, test, type = "class")
+pred <- predict(tree, test_set, type = "class")
 
 # Construye la matriz de confusión
-conf <- table(test$Survived, pred)
+conf <- table(test_set$Supersale, pred)
 
 # Calcula accuracy
 acc <- sum(diag(conf)) / sum(conf)
